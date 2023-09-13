8d56ffbb76b281e6486ab1a80a92db7fe5b8efe1
tet1917 <tetsu0224.soccer@icloud.com>
2023-09-13 18:54:20 +0900

    no message

--- react-hooks-tutorial-youtube/src/App.jsx
+++ react-hooks-tutorial-youtube/src/App.jsx
@@ -28,10 +28,24 @@ function App() {
   const handleRef = () => {
     console.log(ref.current.offsetHeight);
   }
+
   //useMemo
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  const square = () => {
+    let i =0;
+    while(i < 2) {
+      i++;
+    }
+    return count02 * count02;
+  }
+
+  // カスタムフック
+  const [age,setAge] = useLocalStorage("age",24);
 
   return (
-    <>
+    <div>
     <h1>useState , useEffect</h1>
     <button onClick={handleClick}>➕</button>
     <p>{count}</p>
@@ -47,11 +61,22 @@ function App() {
       <button onClick={handleRef}>useRef</button>
 
     <hr/>
+    <h1>useMemo</h1>
+    <div>カウント1:{count01}</div>
+    <div>カウント2:{count02}</div>
+    <div>結果:{square()}</div>
+    <button onClick={()=>setCount01(count01 + 1)}>➕</button>
+    <button onClick={()=>setCount02(count02 + 1)}>➕</button>
+
+    <hr/>
     <h1>useReducer</h1>
-    <p>カウント:{state}</p>
-    <button onClick={()=>dispatch({type:"increment"})}>➕</button>
-    <button onClick={()=>dispatch({type:"decrement"})}>➖</button>
-    </>
+
+
+    <hr/>
+    <h1>カスタムフック</h1>
+    <p>{age}</p>
+    <button onClick={()=>setAge(80)}>年齢をセット</button>
+    </div>
     
   )
 }
