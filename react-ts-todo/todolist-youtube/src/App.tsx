bd84a718c393abe04bdbb9ddf6d951cca9544a1b
tet1917 <tetsu0224.soccer@icloud.com>
2023-09-14 12:36:24 +0900

    no message

---
+++ react-ts-todo/todolist-youtube/src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [inputValue,setInputValue] = useState("");
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue:string;
+    id:number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // enterによってリロードされない
+    e.preventDefault()
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id:todos.length,
+      checked:false
+    };
+    setTodos([newTodo,...todos]);
+    setInputValue("");
+  };
+
+  const handleEdit = (id:number,inputValue:string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Todoリスト with TypeScript</h2>
+        <form onSubmit={(e)=> handleSubmit(e)}>
+          <input type="text" onChange={(e)=>handleChange(e)} className='inputText'/>
+          <input type="submit" value='作成' className='submitButton' />
+        </form>
+        <ul className='todoList'>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="text" onChange={(e)=>handleEdit(todo.id, e.target.value)} className='inputText' value={todo.inputValue}/>
+              <input type="checkbox" onChange={(e)=>handleChecked(todo.id, e.target.value)} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
